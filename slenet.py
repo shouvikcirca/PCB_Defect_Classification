cc3264960391ed37a93d753e301f6bcc0d4818a9
rhettscronfinkle <goldenfibo1123@gmail.com>
2020-04-17 22:15:08 +0530

    Modified slenet.py to take cmd args

--- slenet.py
+++ slenet.py
@@ -8,20 +8,30 @@ import pickle
 import math
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
+import sys
 
 
-
-
+# Taking location of datasets as command line arguments
+xloc = sys.argv[1]
+yloc = sys.argv[2]
 
 
 
 #Loading from numpy files
-X = np.load('drive/My Drive/Copy of s_x_train.npy')
-y = np.load('drive/My Drive/Copy of s_y_train.npy')
+X_train = np.load(xloc)
+y_train = np.load(yloc)
+print('Loading dataset...')
+print(X_train.shape)
+
+
+
 
 #Converting to pytorch tensors
-X = torch.from_numpy(X)
-X = X.permute(0,3,1,2)
+X_train = torch.from_numpy(X_train)
+X_train = X_train.permute(0,3,1,2)
+y_train = torch.from_numpy(y_train)
+print('Converting to Pytorch tensors...')
+
 
 
 # Normalizing image to  0 mean and 1 stddev
@@ -42,6 +52,9 @@ def getNormalized(X,s):
     return temptwo
 
 
+
+
+
 # Resizing image
 def imageSetResize(newSize,X):
     preprocess1 = transforms.Compose([
@@ -57,27 +70,14 @@ def imageSetResize(newSize,X):
     return temp 
 
 
-# To shuffle and create batches at the beginning of each epoch
-def shuffle_and_batch(X,y,num,bs):
-    shuffled_indices = torch.randperm(X.shape[0])
-    newX = X[shuffled_indices]
-    newY = y[shuffled_indices]
-
-    X_batches = []
-    y_batches = []
-    for i in range(num):
-        X_batches.append(X[i*bs:(i+1)*bs])
-        y_batches.append(y_train[i*bs:(i+1)*bs])
-
-    return X_batches, y_batches
-
 
 
 #Resizing the images to 32x32 and normalizing
 imsize = 32
 X_train = imageSetResize(imsize, X_train.float())
-X = getNormalized(X.float(),imsize)
-
+print('Resizing images...')
+X_train = getNormalized(X_train.float(),imsize)
+print('Normalizing images...')
 
 
 
@@ -102,8 +102,9 @@ class lenetModel(nn.Module):
     def __init__(self):
         super(lenetModel, self).__init__()
         self.c1 = nn.Conv2d(3,6, kernel_size = 5)
-        self.pool1 = nn.AvgPool2d(2, stride=2)
+        self.pool1 = nn.MaxPool2d(2, stride=2)
         self.bn1 = nn.BatchNorm2d(6)
+     
         self.first_cons6_filterlist = nn.ModuleList([])
         self.second_cons6_filterlist = nn.ModuleList([])
         self.third_cons3_filterlist = nn.ModuleList([])
@@ -117,10 +118,12 @@ class lenetModel(nn.Module):
             self.third_cons3_filterlist.append(nn.Conv2d(4,1,kernel_size = 5))
         self.fourth_last1_filterlist.append(nn.Conv2d(6,1,kernel_size = 5))
         
-        self.pool2 = nn.AvgPool2d(2, stride=2)
+        self.pool2 = nn.MaxPool2d(2, stride=2)
         self.bn2 = nn.BatchNorm2d(16)
+       
         self.conv3 = nn.Conv2d(16, 120, kernel_size = 5)
         self.bn3= nn.BatchNorm2d(120)
+        
         self.ll1 = nn.Linear(120, 84)
         self.ll2 = nn.Linear(84,2)
         
@@ -131,7 +134,7 @@ class lenetModel(nn.Module):
         nos = x.shape[0]
         ef = lambda x,y: torch.index_select(y,1,torch.tensor(x))
         c1_out = self.c1(x)
-        out = F.relu(self.bn1(self.pool1(c1_out)))
+        out = F.relu(self.bn1(self.pool1(c1_out))) #put bn1
         lione = [ef([0,1,2],out),ef([1,2,3],out), ef([2,3,4],out), ef([3,4,5],out), ef([0,4,5],out),ef([0,1,5],out)]
         litwo = [ef([0,1,2,3],out),ef([1,2,3,4],out), ef([2,3,4,5],out), ef([0,3,4,5],out), ef([0,1,4,5],out),ef([0,1,2,5],out)]
         lithree = [ef([0,1,3,4],out),ef([1,2,4,5],out), ef([0,2,3,5],out)]
@@ -152,11 +155,12 @@ class lenetModel(nn.Module):
         fms.extend(feature_maps1)
         fms.extend(feature_maps2)
         fms.extend(feature_maps3)
+        fms.extend(feature_maps4)
         tfms = torch.Tensor([])
         for i in fms:
             tfms = torch.cat([tfms, i], dim=1)
-        c2_out = F.relu(self.bn2(self.pool2(tfms)))
-        c3_out = self.bn3(self.conv3(c2_out))
+        c2_out = F.relu(self.bn2(self.pool2(tfms))) # put bn2
+        c3_out = self.bn3(self.conv3(c2_out)) #put bn3
         c3_out = c3_out.reshape(nos,120)
         ll1_out = F.relu(self.ll1(c3_out))
         ll2_out = self.ll2(ll1_out)
@@ -167,26 +171,33 @@ class lenetModel(nn.Module):
 
 
 
-
 #Loading trained model
-model_path = './models/10.pth'
+model_path = './model/10.pth'
 a1 = lenetModel()
 n1 = torch.load(model_path)
 a1.load_state_dict(n1)
 a1.eval()
 
 
+print('Loading model...')
 
-raw_preds = a1(Xraw)
-raw_preds = labelize(raw_preds)
-raw_prediction_comparisons = (yraw == raw_preds)
-raw_accuracy = float(raw_prediction_comparisons.sum())/float(yraw.shape[0])
-print('RawAccuracy:{}'.format(raw_accuracy)) 
-print('Raw Dataset Confusion Matrix')
-print(confusion_matrix(yraw, raw_preds))
-print('Raw Dataset F1-Score: ', end='')
-print(f1_score(yraw, raw_preds, average = 'weighted'))
-print('0 Accuracy: '+str(confusion_matrix(yraw, raw_preds)[0][0]/confusion_matrix(yraw, raw_preds)[0].sum()*100)+' %')
-print('1 Accuracy: '+str(confusion_matrix(yraw, raw_preds)[1][1]/confusion_matrix(yraw, raw_preds)[1].sum()*100)+' %')
+
+
+
+# print(a1)
+
+print()
+print()
+preds = a1(X_train)
+preds = labelize(preds)
+prediction_comparisons = (y_train == preds)
+accuracy = float(prediction_comparisons.sum())/float(y_train.shape[0])
+print('Accuracy:{}'.format(accuracy)) 
+print('Confusion Matrix')
+print(confusion_matrix(y_train, preds))
+print('F1-Score: ', end='')
+print(f1_score(y_train, preds, average = 'weighted'))
+print('0 Accuracy: '+str(confusion_matrix(y_train, preds)[0][0]/confusion_matrix(y_train, preds)[0].sum()*100)+' %')
+print('1 Accuracy: '+str(confusion_matrix(y_train, preds)[1][1]/confusion_matrix(y_train, preds)[1].sum()*100)+' %')
 print()
 
